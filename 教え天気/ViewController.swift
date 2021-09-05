33dc883cd2a0169b7af5877a1900bbec8a914e53
justine wright <wright.justine21@gmail.com>
2021-09-05 18:00:41 +0200

    adds mainview to viewcontroller

--- 教え天気/ViewController.swift
+++ 教え天気/ViewController.swift
@@ -2,15 +2,20 @@
 //  ViewController.swift
 //  教え天気
 //
-//  Created by Justine Wright on 2021/09/05.
+//  Created by Justine Wright on 2021/08/27.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    private var mainView: MainView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainView = MainView()
+        view = mainView
+        view.topColouredBlack()
         // Do any additional setup after loading the view.
     }
 
