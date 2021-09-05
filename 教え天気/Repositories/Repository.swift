05300f75b9121e73665d180dc1cbe025bead145c
justine wright <wright.justine21@gmail.com>
2021-09-05 17:59:04 +0200

    adds repos

---
+++ 教え天気/Repositories/Repository.swift
@@ -0,0 +1,44 @@
+//
+//  RepositoryServices.swift
+//  教え天気
+//
+//  Created by Justine Wright on 2021/08/31.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+class Repository {
+    @Published var weatherForecast: [WeatherModel] = []
+    let city = PassthroughSubject<String, Never>()
+    
+    private var cancellables: Set<AnyCancellable> = []
+    private var locationManager: AnyCancellable!
+    private var weatherApiClient: WeatherApiClientProtocol!
+
+    init(weatherApiClient: WeatherApiClientProtocol = OpenWeatherMapsApiClient()) {
+        self.weatherApiClient = weatherApiClient
+        weatherApiClient.listen()
+            .map{ weatherData in
+                self.parseJSON(weatherData: weatherData)
+            }
+            .assign(to: \.weatherForecast, on: self)
+            .store(in: &cancellables)
+
+        locationManager = CLLocationManager.publishLocation()
+            .sink(receiveValue: { self.onLocationUpdate(coordinates: $0) })
+    }
+
+    private func onLocationUpdate(coordinates: CLLocationCoordinate2D) {
+        weatherApiClient.fetch(long: coordinates.longitude, lat: coordinates.latitude)
+        locationManager.cancel()
+    }
+
+    private func parseJSON(weatherData: WeatherData) -> [WeatherModel] {
+        weatherForecast = []
+        let timezone = weatherData.timezone.split(separator: "/")
+        city.send(String(timezone[1]))
+        return weatherData.daily.compactMap(WeatherModel.init)
+    }
+}
---
+++ 教え天気/Repositories/WeatherRepository.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherRepository.swift
+//  教え天気
+//
+//  Created by Justine Wright on 2021/08/31.
+//
+
+import Foundation
+
+class WeatherRepository: RepositoryServices {
+    init(weatherApiClient: WeatherApiClientProtocol = ) {
+        <#statements#>
+    }
+}
