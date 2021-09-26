ec0f935315b7ba994a46b6ff33d89dd074ebb9f4
justine wright <wright.justine21@gmail.com>
2021-09-26 19:55:37 +0200

    deleted main storyboard

--- 教え天気/ApplicationLayer/SceneDelegate.swift
+++ 教え天気/ApplicationLayer/SceneDelegate.swift
@@ -17,10 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
-        initialViewController = storyboard.instantiateViewController(withIdentifier: "ViewController") 
-
+        initialViewController = MainRouter.createModule()
+        _ = Database.shared
+        _ = LocationTracker.shared
+        
         window = UIWindow(windowScene: windowScene)
         window!.rootViewController = initialViewController
         window!.makeKeyAndVisible()
